97809cca831883820f5f43b3ae8d4240bc3b6e67
Sergey <104456968+Solod-S@users.noreply.github.com>
2022-10-29 19:48:26 +0300

    fix_logo_link

--- src/js/moviesListRenderByTopAndSearch.js
+++ src/js/moviesListRenderByTopAndSearch.js
@@ -1,26 +1,6 @@
 import TmDbApi from "./services/fetchApi";
 import genres from "./services/genres";
 import { modalListener } from "./modal";
-import Handlebars from "handlebars";
-console.log(Handlebars);
-const template = Handlebars.compile(``);
-console.log(
-  template(`
-    <div class="color-card">
-      <div
-        class="color-swatch"
-        data-hex="{{hex}}"
-        data-rgb="{{rgb}}"
-        style="background-color: {{hex}}"
-      ></div>
-      <div class="color-meta">
-        <p>HEX: {{ hex }}</p>
-        <p>RGB: {{ rgb }}</p>
-      </div>
-    </div>`)
-);
-// import colorCardTpl from "../templates/color-card.hbs";
-// console.log(template(`../templates/color-card.hbs`));
 const Api = new TmDbApi();
 export const defaultPoster =
   "https://tn.fishki.net/26/upload/post/2018/04/20/2577020/afrikanskie-postery-k-gollivudskim-blokbasteram-6.jpg";
@@ -36,7 +16,6 @@ export const moviesListRenderByTopAndSearch = {
     query: "",
     page: 1,
     totalPages: null,
-    // per_page: 15,
   },
   onSearchForm(event) {
     event.preventDefault();
@@ -186,11 +165,10 @@ moviesListRenderByTopAndSearch.searchForm.addEventListener(
 
 moviesListRenderByTopAndSearch.logo.addEventListener("click", () => {
   moviesListRenderByTopAndSearch.options.query = "";
+  moviesListRenderByTopAndSearch.options.page = 1;
   moviesListRenderByTopAndSearch.render();
 });
 
 moviesListRenderByTopAndSearch.navigationHomeBtn.addEventListener(`click`, () =>
   moviesListRenderByTopAndSearch.render()
 );
-
-console.log(Api.fetchMovieTrailer(11));
